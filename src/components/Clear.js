947e094a209f2193eea35c714f9d242fe47ac9b9
C-Rodg <rodgersbc@gmail.com>
2017-11-06 11:44:10 -0800

    Started clear device screen

--- src/components/Clear.js
+++ src/components/Clear.js
@@ -1,34 +1,82 @@
 import React, { Component } from "react";
+import moment from "moment";
 
 import HomeButton from "./HomeButton";
 import "../styles/clear.css";
 
 class Clear extends Component {
+	constructor() {
+		super();
+		this.state = {
+			currentDate: moment(),
+			offset: 0
+		};
+		this.interval = setInterval(this.updateDateTick, 1000);
+	}
+
+	// Clear timer
+	componentWillUnmount() {
+		clearInterval(this.interval);
+	}
+
+	// Update the current date
+	updateDateTick = () => {
+		let currentDate;
+		if (this.state.offset >= 0) {
+			currentDate = moment().add(this.state.offset, "h");
+		} else {
+			currentDate = moment().subtract(Math.abs(this.state.offset), "h");
+		}
+		this.setState({
+			currentDate
+		});
+	};
+
+	// Add or subtract hour from device time
+	modifyTime = isAdd => {
+		const offset = isAdd ? this.state.offset + 1 : this.state.offset - 1;
+		this.setState({
+			offset
+		});
+	};
+
 	render() {
 		return (
 			<div className="clear container">
 				<div className="row">
 					<div className="card">
-						<div className="card-title">Device Information:</div>
+						<div className="card-title">Initialization Settings:</div>
 						<div className="card-row">
-							<span className="card-info-title">Total Scans:</span>
-							<span className="card-info-answer">328 scans</span>
+							<span className="card-info-answer">
+								Are you sure you want to clear <b>ALL</b> data and reset the
+								device time? This action cannot be undone.
+							</span>
 						</div>
-						<div className="card-row">
-							<span className="card-info-title">Unique Scans:</span>
-							<span className="card-info-answer">302 scans</span>
-						</div>
-						<div className="card-row">
-							<span className="card-info-title">First Scan:</span>
-							<span className="card-info-answer">4:22 am, Jun 28th, 2017</span>
-						</div>
-						<div className="card-row">
-							<span className="card-info-title">Last Scan:</span>
-							<span className="card-info-answer">7:22 pm, Oct 14th, 2017</span>
-						</div>
-						<div className="card-row">
-							<span className="card-info-title">Session Scans Present?</span>
-							<span className="card-info-answer">Yes (14 sessions)</span>
+						<div className="center-section">
+							<div className="card-row">
+								<span className="card-info-title">Scans to be Deleted:</span>
+								<span className="card-info-answer">372 scans</span>
+							</div>
+							<div className="card-row">
+								<span className="card-info-title">New Device Time:</span>
+								<span className="card-info-answer">
+									{this.state.currentDate.format("h:mm:ss A, MMM Do, YYYY")}
+								</span>
+							</div>
+							<div className="card-row mod-time">
+								<span
+									className="time-action"
+									onClick={() => this.modifyTime(true)}
+								>
+									+1 Hour
+								</span>
+								<span
+									className="time-action"
+									onClick={() => this.modifyTime(false)}
+								>
+									-1 Hour
+								</span>
+							</div>
 						</div>
 					</div>
 				</div>
